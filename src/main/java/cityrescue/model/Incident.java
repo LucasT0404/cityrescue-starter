1423c4c309b84abbe494788a956ea948b09f960e
LucasT0404 <ltd202@exeter.ac.uk>
2026-02-28 23:23:48 +0000

    restore model classes

---
+++ src/main/java/cityrescue/model/Incident.java
@@ -0,0 +1,37 @@
+package cityrescue.model;
+
+import cityrescue.enums.IncidentStatus;
+import cityrescue.enums.IncidentType;
+
+public class Incident {
+
+    private final int incidentId;
+    private final IncidentType type;
+    private int severity;
+    private final int x;
+    private final int y;
+    private IncidentStatus status;
+    private int assignedUnitId;
+
+    public Incident(int incidentId, IncidentType type, int severity, int x, int y) {
+        this.incidentId = incidentId;
+        this.type = type;
+        this.severity = severity;
+        this.x = x;
+        this.y = y;
+        this.status = IncidentStatus.REPORTED;
+        this.assignedUnitId = -1;
+    }
+
+    public int getIncidentId()         { return incidentId; }
+    public IncidentType getType()      { return type; }
+    public int getSeverity()           { return severity; }
+    public int getX()                  { return x; }
+    public int getY()                  { return y; }
+    public IncidentStatus getStatus()  { return status; }
+    public int getAssignedUnitId()     { return assignedUnitId; }
+
+    public void setSeverity(int severity)          { this.severity = severity; }
+    public void setStatus(IncidentStatus status)   { this.status = status; }
+    public void setAssignedUnitId(int unitId)      { this.assignedUnitId = unitId; }
+}
---
+++ src/main/java/cityrescue/model/Station.java
@@ -0,0 +1,34 @@
+package cityrescue.model;
+
+public class Station {
+
+    private final int stationId;
+    private final String name;
+    private final int x;
+    private final int y;
+    private int maxUnits;
+    private int unitCount;
+
+    public Station(int stationId, String name, int x, int y, int maxUnits) {
+        this.stationId = stationId;
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.maxUnits = maxUnits;
+        this.unitCount = 0;
+    }
+
+    public int getStationId() { return stationId; }
+    public String getName() { return name; }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getMaxUnits() { return maxUnits; }
+    public int getUnitCount() { return unitCount; }
+
+    public void setMaxUnits(int maxUnits) { this.maxUnits = maxUnits; }
+
+    public boolean hasCapacity() { return unitCount < maxUnits; }
+
+    public void incrementUnitCount() { unitCount++; }
+    public void decrementUnitCount() { unitCount--; }
+}
